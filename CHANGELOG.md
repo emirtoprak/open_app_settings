90bc785a90b98bfbe51c75627f154b6a618830de
Emir Toprak <emir@cntbilisim.com.tr>
2025-12-24 17:20:54 +0300

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+
+## 2.0.1
+* Add support for android constant values
+## 2.0.0-nullsafety.1
+* The Flutter constraint should not have an upper bound.
+## 1.0.0
+* init.
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<open_app_settings/OpenAppSettingsPlugin.h>)
+#import <open_app_settings/OpenAppSettingsPlugin.h>
+#else
+@import open_app_settings;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [OpenAppSettingsPlugin registerWithRegistrar:[registry registrarForPlugin:@"OpenAppSettingsPlugin"]];
+}
+
+@end
