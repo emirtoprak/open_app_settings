b37c56d8545082427748aee3f0275078a32cb7b6
Emir Toprak <emir@cntbilisim.com.tr>
2025-12-25 11:54:57 +0300

    [feature] agp 12 updated

---
+++ android/build/generated/source/buildConfig/release/com/oversketch/open_app_settings/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.oversketch.open_app_settings;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.oversketch.open_app_settings";
+  public static final String BUILD_TYPE = "release";
+}
